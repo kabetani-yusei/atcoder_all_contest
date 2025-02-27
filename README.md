57a1b44ebe9f57c7a7d672cb0da7e7816dd903af
kabetani-yusei <yuusei1771@gmail.com>
2025-02-27 22:56:15 +0900

    abc212

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # acc 使い方
 ```
-acc new abc214
+acc new abc212
+
+g++ main.cpp -std=c++17 -I .
 oj t -c "./a.out" -d ./test/  
 ```
 ## 参考url
 https://kankisenkowasuo.hatenablog.com/entry/2024/09/25/001415
+
+## いつもの
+```
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+```
---
+++ abc212/d/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll inf = 1e18;
+const int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+const int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+#define bsearch(a,x) distance(a.begin(), lower_bound(all(a), x))
+#define all(a) a.begin(),a.end()
+#define overload(_1,_2,_3,_4,name,...) name
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+template <class T> bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
+template <class T> bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
+struct Edge { int to; ll cost; Edge(int to, ll cost) : to(to), cost(cost) {} };
+using Graph = vector<vector<Edge>>;
+
+int main(){
+    int q;
+    cin >> q;
+    ll a,b,total=0;
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    rep(q){
+        cin >> a;
+        if(a == 1){
+            cin >> b;
+            b -= total;
+            pq.push(b);
+        }else if(a == 2){
+            cin >> b;
+            total += b;
+        }else{
+            cout << pq.top() + total << endl;
+            pq.pop();
+        }
+    }
+}
