43ed425347fc0731499fbb8680af78c97de38d7b
kabetani-yusei <yuusei1771@gmail.com>
2025-03-11 21:17:28 +0900

    座標圧縮

---
+++ mylibrary/座標圧縮.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int comp(vector<int> &A){
+    std::map<int,int> mem;
+    for(auto p: A) mem[p] = 0;
+    int sz = 0;
+    for(auto &p: mem) p.second = sz++;
+    for(auto &p: A) p = mem[p];
+    return sz;
+}
