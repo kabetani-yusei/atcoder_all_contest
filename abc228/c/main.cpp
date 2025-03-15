72072e717a73d28c33094dd8db4be3c2cbfe9655
kabetani-yusei <yuusei1771@gmail.com>
2025-03-15 13:02:17 +0900

    abc228

---
+++ abc228/c/main.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint998244353;
+#define ll long long
+#define ull unsigned long long
+const ll inf = 1e18;
+const int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+const int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+#define bsearch(a,x) distance(a.begin(), lower_bound(all(a), x))
+#define all(a) a.begin(),a.end()
+#define overload(_1,_2,_3,_4,name,...) name
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+template <class T> bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
+template <class T> bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
+struct Edge { int to; ll cost; Edge(int to, ll cost) : to(to), cost(cost) {} };
+using Graph = vector<vector<Edge>>;
+// cout << fixed << setprecision(15);
+
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+    vector<vector<int>> a(n, vector<int>(3));
+    rep(n) cin >> a[i][0] >> a[i][1] >> a[i][2];
+    vector<int> now;
+    rep(n){
+        now.push_back(a[i][0]+a[i][1]+a[i][2]);
+    }
+    sort(all(now), greater<int>());
+    rep(n){
+        int tt = a[i][0]+a[i][1]+a[i][2]+300;
+        if(tt >= now[0]){
+            cout << "Yes" << endl;
+            continue;
+        }
+        int ok=0,ng=n-1;//どの要素の右にいれるか
+        while(ng-ok > 1){
+            int m = (ok+ng)/2;
+            if(now[m] > tt) ok = m;
+            else ng = m;
+        }
+        if(ok < k - 1){
+            cout << "Yes" << endl;
+        }else{
+            cout <<  "No" << endl;
+        }
+    }
+}   
