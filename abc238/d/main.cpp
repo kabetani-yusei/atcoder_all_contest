ef6d6216cec2762206725fbeab67f55aa17f3d0d
kabetani-yusei <yuusei1771@gmail.com>
2025-03-25 22:34:34 +0900

    abc238

---
+++ abc238/d/main.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+// using mint = modint998244353;
+#define ll long long
+#define ull unsigned long long
+const ll inf = 1e18;
+const int dx[] = {0, 1, 0, -1, 1, 1, -1, -1};
+const int dy[] = {1, 0, -1, 0, 1, -1, 1, -1};
+#define bsearch(a,x) distance(a.begin(), lower_bound(all(a), x))
+#define all(a) a.begin(),a.end()
+#define overload(_1,_2,_3,_4,name,...) name
+#define _rep1(n) for(int i = 0; i < (n); i++)
+#define _rep2(i,n) for(int i = 0; i < (n); i++)
+#define _rep3(i,a,b) for(int i = (a); i < (b); i++)
+#define _rep4(i,a,b,c) for(int i = (a); i < (b); i += (c))
+#define rep(...) overload(__VA_ARGS__,_rep4,_rep3,_rep2,_rep1)(__VA_ARGS__)
+#define _rrep1(n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep2(i,n) for(int i = (n) - 1; i >= 0; i--)
+#define _rrep3(i,a,b) for(int i = (b) - 1; i >= (a); i--)
+#define _rrep4(i,a,b,c) for(int i = (b) - 1; i >= (a); i -= (c))
+#define rrep(...) overload(__VA_ARGS__,_rrep4,_rrep3,_rrep2,_rrep1)(__VA_ARGS__)
+template <class T> bool chmin(T& a, T b){ if(a > b){ a = b; return 1; } return 0; }
+template <class T> bool chmax(T& a, T b){ if(a < b){ a = b; return 1; } return 0; }
+struct Edge { int to; ll cost; Edge(int to, ll cost) : to(to), cost(cost) {} };
+using Graph = vector<vector<Edge>>;
+// cout << fixed << setprecision(15);
+
+
+int main(){
+    int t;
+    cin >> t;
+    rep(ttt, t){
+        ll a,s;
+        cin >> a >> s;
+        // 二進数にして逆からにする
+        string sa = bitset<64>(a).to_string();
+        reverse(all(sa));
+        string ss = bitset<64>(s).to_string();
+        reverse(all(ss));
+        int flag = 0;
+        bool ok = true;
+        rep(64){
+            if(sa[i] == '1'){
+                if(ss[i] != flag + '0'){
+                    ok = false;
+                    break;
+                }
+                flag = 1;
+            }else{
+                if(ss[i] == '1'){
+                    flag = 0;
+                }
+            }
+        }
+        if(flag == 0 && ok){
+            cout << "Yes" << endl;
+        }else{
+            cout << "No" << endl;
+        }
+    }
+}   
