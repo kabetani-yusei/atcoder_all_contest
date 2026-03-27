453622a2250c4eff3ac6fdc83e214ee2cb06a05d
kabetani-yusei <yuusei1771@gmail.com>
2026-03-27 22:01:21 +0900

    1週目: abc234まで

--- mylibrary/k進数.cpp
+++ mylibrary/k進数.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
 // 10進数をk進数に変える（kが10以下の場合）
-string toBaseK(int n, int k) {
+string toBaseK(ll n, int k) {
     string result = "";
     while (n > 0) {
         int r = n % k;
@@ -13,8 +14,8 @@ string toBaseK(int n, int k) {
 }
 
 // k進数を10進数に変える（kが10以下の場合）
-int toDecimal(string s, int k) {
-    int result = 0;
+ll toDecimal(string s, int k) {
+    ll result = 0;
     for (char c : s) {
         int digit = c - '0'; // '0'〜'9'前提
         result = result * k + digit;
@@ -28,8 +29,8 @@ kが10より大きい場合
 */
 
 // 10新しい数 -> k進数
-string toBaseK(int n, int k) {
-    if (n == 0) return "0";
+string toBaseK(ll n, int k) {
+    if (n == 0LL) return "0";
 
     string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     string result = "";
@@ -50,8 +51,8 @@ int charToInt(char c) {
     return -1;
 }
 
-int toDecimal(string s, int k) {
-    int result = 0;
+ll toDecimal(string s, int k) {
+    ll result = 0;
 
     for (char c : s) {
         int digit = charToInt(c);
